7c4b1a53554fd415fa13489d6c53ccdca6427bd6
ninglisha <lilysa2604@qq.com>
2018-04-03 12:21:52 +0800

    4-3 test

--- CloundSafe/Sources/Controller/MyInfo/CSMyInfoController.m
+++ CloundSafe/Sources/Controller/MyInfo/CSMyInfoController.m
@@ -29,8 +29,8 @@
     [super viewDidLoad];
     
     [self initTableView];
-    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:20.0/255.0 green:205.0/255.0 blue:111.0/255.0 alpha:1.0];
-    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+//    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:20.0/255.0 green:205.0/255.0 blue:111.0/255.0 alpha:1.0];
+//    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
 }
 - (void)initTableView
 {
